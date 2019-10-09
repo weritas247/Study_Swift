44ec34a62465ee653c96df2be0679e5b4e37e17f
LSH <secondwarren@gmail.com>
2019-10-10 01:10:08 +0900

    GCD(GooglyPuff) 완료

--- study_swift/GCD/GooglyPuff/GooglyPuffTests/GooglyPuffTests.swift
+++ study_swift/GCD/GooglyPuff/GooglyPuffTests/GooglyPuffTests.swift
@@ -1,33 +1,50 @@
 //
 //  GooglyPuffTests.swift
-//  GooglyPuffTests
+//  study_swift
 //
-//  Created by MyMacBookPro on 10/10/2019.
+//  Created by MyMacBookPro on 09/10/2019.
 //  Copyright © 2019 Maverick DevStudioa. All rights reserved.
 //
 
 import XCTest
+@testable import study_swift
+
+private let defaultTimeoutLengthInSeconds: Int = 10 // 10 Seconds
 
 class GooglyPuffTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testLotsOfFacesImageURL() {
+        downloadImageURL(withString: PhotoURLString.lotsOfFaces)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testSuccessKidImageURL() {
+        downloadImageURL(withString: PhotoURLString.successKid)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testOverlyAttachedGirlfriendImageURL() {
+        downloadImageURL(withString: PhotoURLString.overlyAttachedGirlfriend)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func downloadImageURL(withString urlString: String) {
+        
+        let url = URL(string: urlString)
+        
+        // 1 - You create a semaphore and set its start value. This represents the number of things that can access the semaphore without needing the semaphore to be incremented (note that incrementing a semaphore is known as signaling it).
+        let semaphore = DispatchSemaphore(value: 0)
+        let _ = DownloadPhoto(url: url!) { _, error in
+            if let error = error {
+                XCTFail("\(urlString) failed. \(error.localizedDescription)")
+            }
+            
+            // 2 - You signal the semaphore in the completion closure. This increments the semaphore count and signals that the semaphore is available to other resources that want it.
+            semaphore.signal()
+        }
+        let timeout = DispatchTime.now() + .seconds(defaultTimeoutLengthInSeconds)
+        
+        // 3 - You wait on the semaphore, with a given timeout. This call blocks the current thread until the semaphore is signaled. A non-zero return code from this function means that the timeout period expired. In this case, the test fails because the network should not take more than 10 seconds to return — a fair point!
+        if semaphore.wait(timeout: timeout) == .timedOut {
+            XCTFail("\(urlString) timed out")
         }
     }
-
+   
 }
--- study_swift/GCD/GooglyPuff/PhotoCollectionViewController.swift
+++ study_swift/GCD/GooglyPuff/PhotoCollectionViewController.swift
@@ -7,11 +7,38 @@ import Photos
 private let reuseIdentifier = "photoCell"
 private let backgroundImageOpacity: CGFloat = 0.1
 
+// 1 - You compile this code only in DEBUG mode to prevent "interested parties" from gaining a lot of insight into your app. :] DEBUG is defined by adding -D DEBUG under Project Settings -> Build Settings -> Swift Compiler - Custom Flags -> Other Swift Flags -> Debug. It should be set already in the starter project.
+#if DEBUG
+
+// 2 - You declare a signal variable of type DispatchSourceSignal for use in monitoring Unix signals.
+var signal: DispatchSourceSignal?
+
+// 3 - You create a block assigned to the setupSignalHandlerFor global variable that you'll use for one-time setup of your dispatch source.
+private let setupSignalHandlerFor = { (_ object: AnyObject) in
+    let queue = DispatchQueue.main
+    
+    // 4 - Here you set up signal. You indicate that you're interested in monitoring the SIGSTOP Unix signal and handling received events on the main queue — you'll discover why shortly.
+    signal = DispatchSource.makeSignalSource(signal: SIGSTOP, queue: queue)
+    
+    // 5 - If the dispatch source is successfully created, you register an event handler closure that's invoked whenever you receive the SIGSTOP signal. Your handler prints a message that includes the class description.
+    signal?.setEventHandler{
+        print("Hi, I am: \(object.description!)")
+    }
+    
+    // 6 - All sources start off in the suspended state by default. Here you tell the dispatch source to resume so it can start monitoring events.
+    signal?.resume()
+}
+#endif
+
 final class PhotoCollectionViewController: UICollectionViewController {
   
   // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    #if DEBUG
+    setupSignalHandlerFor(self)
+    #endif
     
     let backgroundImageView = UIImageView(image: UIImage(named:"background"))
     backgroundImageView.alpha = backgroundImageOpacity
